dd5c375a92e6a2cea884500c7f8061e6b10f9a85
Ahmet_Emin <ahmeteminsaglik@gmail.com>
2023-03-17 01:26:50 +0300

    Able to get data from web service in kotlin.

--- BookDemoAPI/src/main/java/com/ahmeteminsaglik/bookdemoapi/controller/BookController.java
+++ BookDemoAPI/src/main/java/com/ahmeteminsaglik/bookdemoapi/controller/BookController.java
@@ -3,6 +3,7 @@ package com.ahmeteminsaglik.bookdemoapi.controller;
 import com.ahmeteminsaglik.bookdemoapi.business.ExampleData;
 import com.ahmeteminsaglik.bookdemoapi.model.Book;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,5 +17,10 @@ public class BookController {
     @GetMapping("")
     public List<Book> getBookList() {
         return exampleData.getBookList();
+    }
+
+    @GetMapping("/{bookId}")
+    public Book getBookById(@PathVariable int bookId) {
+        return exampleData.getBookList().get(bookId-1);
     }
 }
