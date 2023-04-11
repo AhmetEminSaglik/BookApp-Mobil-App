4403b1366d4d993386f4e9118db3406232769a38
Ahmet_Emin <ahmeteminsaglik@gmail.com>
2023-04-11 07:16:04 +0300

    The data query links are given in the order in which they need to be executed

--- neo4j-social-medya/src/main/java/com/ahmeteminsaglik/neo4jsocialmedya/controller/DataCreationController.java
+++ neo4j-social-medya/src/main/java/com/ahmeteminsaglik/neo4jsocialmedya/controller/DataCreationController.java
@@ -6,23 +6,51 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/data/")
+@RequestMapping("/data")
 public class DataCreationController {
     DataCreation dataCreation = new DataCreation();
 
-    @GetMapping("create")
+    @GetMapping
+    public String getLinks() {
+        String text = "Please run the codes in the links provided in the given order.\n<ul>";
+        text += getHTMLLink("create", "create data query");
+        text += getHTMLLink("fix-user-data", "fix user data query");
+        text += getHTMLLink("fix-book-data", "fix book data query");
+        text += getHTMLLink("fix-author-data", "fix author data query");
+        text += "</ul>";
+        return text;
+    }
+
+    @GetMapping("/create")
     public String getRandomNeo4jData() {
         return getHTMLCode(dataCreation.getQueryText().toString());
     }
 
-    @GetMapping("fix")
-    public String getFixDataQuery() {
-        String text = dataCreation.getFixDataQueryText().toString();
+    @GetMapping("/fix-user-data")
+    public String getFixUserDataQuery() {
+        String text = dataCreation.getFixUserDataQueryText().toString();
         return getHTMLCode(text);
     }
+
+    @GetMapping("/fix-book-data")
+    public String getFixBookDataQuery() {
+        String text = dataCreation.getFixBookPointDataQueryText().toString();
+        return getHTMLCode(text);
+    }
+
+    @GetMapping("/fix-author-data")
+    public String getFixAuthorDataQuery() {
+        String text = dataCreation.getFixAuthorDataQueryText().toString();
+        return getHTMLCode(text);
+    }
+
 
     private String getHTMLCode(String text) {
         text = text.replaceAll("\n", "<br>");
         return text;
+    }
+
+    private String getHTMLLink(String link, String explain) {
+        return "<li> <a href=\"http://localhost:8080/data/" + link + "\">" + explain + "</a><br> </li> ";
     }
 }
