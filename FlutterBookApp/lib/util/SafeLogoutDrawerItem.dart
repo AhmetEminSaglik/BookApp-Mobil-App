43b45ad0df2a7d9d270fca7f3aa3bd7208faa481
Ahmet Emin SAGLIK <ahmeteminsaglik@gmail.com>
2023-09-27 01:15:00 +0300

    Safe logout function is created

---
+++ FlutterBookApp/lib/util/SafeLogoutDrawerItem.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+import 'SharedPrefUtils.dart';
+
+class SafeLogoutDrawerItem extends StatefulWidget {
+  @override
+  State<SafeLogoutDrawerItem> createState() => _SafeLogoutDrawerItemState();
+}
+
+class _SafeLogoutDrawerItemState extends State<SafeLogoutDrawerItem> {
+  @override
+  Widget build(BuildContext context) {
+    return _buildDrawerListTileSafeLogout(context: context);
+  }
+
+  ListTile _buildDrawerListTileSafeLogout({required BuildContext context}) {
+    return ListTile(
+      title: const Text("Safe Logout"),
+      onTap: () {
+        setState(() {
+          SafeLogOut.clearSharedPref();
+          Navigator.of(context).popUntil((route) => route.isFirst);
+        });
+      },
+    );
+  }
+}
+
+class SafeLogOut {
+  static void clearSharedPref() {
+    SharedPrefUtils.sp.clear();
+  }
+}
