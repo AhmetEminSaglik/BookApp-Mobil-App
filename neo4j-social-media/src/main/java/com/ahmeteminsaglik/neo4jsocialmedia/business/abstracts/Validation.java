eacf0d16dc7323efb130d344893d0b4fd88d7927
Ahmet Emin SAGLIK <ahmeteminsaglik@gmail.com>
2025-02-07 19:57:36 +0300

    old backend folder and classes are removed

--- neo4j-social-media/src/main/java/com/ahmeteminsaglik/neo4jsocialmedia/business/abstracts/Validation.java
+++
@@ -1,8 +0,0 @@
-package com.ahmeteminsaglik.neo4jsocialmedia.business.abstracts;
-
-import com.ahmeteminsaglik.neo4jsocialmedia.model.User;
-import com.ahmeteminsaglik.neo4jsocialmedia.utility.result.DataResult;
-
-public interface Validation {
-    DataResult<User> validate(User user);
-}
--- neo4j-social-media/src/main/java/com/ahmeteminsaglik/neo4jsocialmedia/utility/exception/InvalidLoginInputException.java
+++
@@ -1,7 +0,0 @@
-package com.ahmeteminsaglik.neo4jsocialmedia.utility.exception;
-
-public class InvalidLoginInputException extends Exception {
-    public InvalidLoginInputException() {
-        super("Invalid username or password.");
-    }
-}
